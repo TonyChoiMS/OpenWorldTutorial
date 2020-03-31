4407f3c3b588778ae43b3328058e3aad4ab240db
TonyChoi <cms9286@gmail.com>
2020-04-01 01:05:12 +0900

    Add ExplosiveBarrel

--- Source/OpenWorldTutorial/MyCharacter.h
+++ Source/OpenWorldTutorial/MyCharacter.h
@@ -52,6 +52,7 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	USpringArmComponent* SpringArmComp;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	USHealthComponent* HealthComp;
 
 	bool bWantsToZoom;
@@ -83,7 +84,7 @@ protected:
 	void StopFire();
 
 	UFUNCTION()
-	void OnHealthChanged(USHealthComponent* HealthComp, float Health, float HealthDelta, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+	void OnHealthChanged(USHealthComponent* OwningHealthComp, float Health, float HealthDelta, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
 
 	// Pawn died previously
 	UPROPERTY(BlueprintReadOnly, Category = "Player")
